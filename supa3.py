6a511c7f16f635a2c72b957679754baee87e9089
jaysys <jaysys@gmail.com>
2024-05-07 22:52:22 +0900

    Changes to be committed: 	modified:   supa3.py

--- supa3.py
+++ supa3.py
@@ -1,33 +1,37 @@
+#postgreSQL 
+
 from sqlalchemy import create_engine, text
 
-# Define the connection string
-connection_string = "postgresql://postgres.jutmv-aawmd:F26-passwd-qo@aws-0-ap-northeast-2.pooler.supabase.com:6543/postgres"
+CONNECTION_STRING = "postgresql://postgres.jutm~~~~awmd:F26~~dbpwd~~qo@aws-0-ap-northeast-2.pooler.supabase.com:6543/postgres"
 
-# Create an engine
-engine = create_engine(connection_string)
-
-# Function to execute SQL query
-def get_query(stmt, engine=engine):
+def conn_db_and_exec_query(stmt):
+    engine = create_engine(CONNECTION_STRING)    
     with engine.connect() as conn:
-        # Execute the query
-        result = conn.execute(text(stmt))
-        return result
+        result_set = conn.execute(text(stmt))
+        return result_set
 
-# Sample query with limit and order by timestamp desc and index asc
-def collect_data():
-    query = """
+def fetch_data_from_exec_query(stmt):  
+    result_set = conn_db_and_exec_query(stmt)
+
+    one = result_set.fetchone()
+    print(one)
+
+    many = result_set.fetchmany(2)
+    print(many)
+    
+    all = result_set.fetchall() #이미 fetch된게 있으면 그 다음부터 전체, fetch된게 없으면 처음부터 전체 데이터 넘겨받음
+    print(all)
+    
+    return (one, many, all)
+
+if __name__ == "__main__":
+    query_stmt = """
         SELECT timestamp, index, div, asset, qty, unit_usd, unit_krw, total_krw 
         FROM my_asset 
         ORDER BY "timestamp" DESC, "index" ASC 
         LIMIT 10
-    """    
-    result = get_query(stmt = query)
-    print(result.fetchone())
-    print(result.fetchmany(2))
-    results = result.fetchall()
-    return results
-
-# Example usage:
-data = collect_data()
-import pprint
-pprint.pprint(data)
+    """  
+    data = fetch_data_from_exec_query(query_stmt)
+    
+    import pprint
+    pprint.pprint(data)
