a9d24217ff7cc5cafa5573859c1e076ea0801625
jaysys <jaysys@gmail.com>
2024-05-07 23:05:32 +0900

    Changes to be committed: 	renamed:    supa3.py -> supa-postgre-select.py 	deleted:    supa2.py

--- supa-postgre-select.py
+++ supa-postgre-select.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import create_engine, text
 
-CONNECTION_STRING = "postgresql://postgres.jutm~~~~awmd:F26~~dbpwd~~qo@aws-0-ap-northeast-2.pooler.supabase.com:6543/postgres"
+CONNECTION_STRING = "postgresql://postgres.ju~~~~md:F26~passwd~~o@aws-0-ap-northeast-2.pooler.supabase.com:6543/postgres"
 
 def conn_db_and_exec_query(stmt):
     engine = create_engine(CONNECTION_STRING)    
@@ -14,17 +14,17 @@ def fetch_data_from_exec_query(stmt):
     result_set = conn_db_and_exec_query(stmt)
 
     one = result_set.fetchone()
-    print(one)
+    #print(one)
 
     many = result_set.fetchmany(2)
-    print(many)
+    #print(many)
     
     all = result_set.fetchall() #이미 fetch된게 있으면 그 다음부터 전체, fetch된게 없으면 처음부터 전체 데이터 넘겨받음
-    print(all)
+    #print(all)
     
     return (one, many, all)
 
-if __name__ == "__main__":
+def use_case_01():
     query_stmt = """
         SELECT timestamp, index, div, asset, qty, unit_usd, unit_krw, total_krw 
         FROM my_asset 
@@ -35,3 +35,30 @@ if __name__ == "__main__":
     
     import pprint
     pprint.pprint(data)
+
+def use_case_02():
+    engine = create_engine(CONNECTION_STRING) 
+    with engine.connect() as connection:
+        # Build the select query
+        select_query = """
+            SELECT * 
+            FROM my_asset 
+            ORDER BY timestamp DESC, index ASC 
+            LIMIT 10
+        """
+        
+        # Execute the query
+        result_set = connection.execute(text(select_query))
+        
+        # Print the results
+        for row in result_set:
+            print(row)
+
+if __name__ == "__main__":
+    print("=-"*10,"start!")
+    use_case_01()
+    print("=-"*10)
+    use_case_02()
+    print("=-"*10,"done!")
+
+
